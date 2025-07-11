1652565c858a129ba121f8568462ce4c0fc464f1
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-07-11 18:22:04 +0200

    Working on BiometrikAuthenticator.wasmJs.kt integration

--- library/src/wasmJsMain/kotlin/com/tecknobit/biometrik/BiometrikAuthenticator.wasmJs.kt
+++ library/src/wasmJsMain/kotlin/com/tecknobit/biometrik/BiometrikAuthenticator.wasmJs.kt
@@ -1,6 +1,7 @@
 package com.tecknobit.biometrik
 
 import androidx.compose.runtime.Composable
+import org.khronos.webgl.Uint8Array
 
 @Composable
 actual fun BiometrikAuthenticator(
@@ -14,4 +15,45 @@ actual fun BiometrikAuthenticator(
     onFeatureUnavailable: @Composable () -> Unit,
     onAuthenticationNotSet: @Composable () -> Unit,
 ) {
+    val challenge = Uint8Array(32)
+    loadChallenge(
+        challenge = challenge
+    )
+    val publicKey = createPublicKey(
+        challenge = challenge
+    )
+    val options = createOptions(
+        publicKey = publicKey
+    )
+    requestAuth(
+        options = options
+    )
 }
+
+private fun loadChallenge(
+    challenge: Uint8Array,
+): Unit = js(
+    """
+        {
+            window.crypto.getRandomValues(challenge)
+        }
+    """
+)
+
+private fun createPublicKey(
+    challenge: Uint8Array,
+): JsAny = js("({ challenge : challenge.buffer, timeout : 60000, userVerification : 'preferred' })")
+
+private fun createOptions(
+    publicKey: JsAny,
+): JsAny = js("({ publicKey : publicKey})")
+
+private fun requestAuth(
+    options: JsAny,
+): Unit = js(
+    """
+        {
+            window.navigator.credentials.get(options).await()
+        }
+    """
+)
