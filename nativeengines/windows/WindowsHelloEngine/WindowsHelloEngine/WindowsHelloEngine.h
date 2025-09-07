8f7d56924c1752b6d1398a4946326e3d79299909
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-09-07 20:49:48 +0200

    Decreased the value of FORCE_PROMPT_ON_TOP_MAX_ATTEMPTS

--- nativeengines/windows/WindowsHelloEngine/WindowsHelloEngine/WindowsHelloEngine.h
+++ nativeengines/windows/WindowsHelloEngine/WindowsHelloEngine/WindowsHelloEngine.h
@@ -32,7 +32,7 @@ extern "C" {
     /**
      * Constant value represents the maximum allowed attempts to force the biometric prompt on top
      */
-    static const int FORCE_PROMPT_ON_TOP_MAX_ATTEMPTS = 20;
+    static const int FORCE_PROMPT_ON_TOP_MAX_ATTEMPTS = 15;
 
     /**
      * @enum AuthenticationResult
